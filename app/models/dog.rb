5c243a2924132751a3ce2a638bdeec75551a0ed3
alexagtn2714 <alexagaton@hotmail.com>
2018-07-17 17:56:15 +0000

    automatically backed up by learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,10 +1,13 @@
 class Dog 
   # Replace with Dog class
-  attr_accessor :name, :breed, :age 
+  attr_accessor :name, :breed, :age
+  @@dog_array = []
   
   def initialize(name,breed,age)
     @name = name 
     @breed = breed 
     @age = age 
+    @@dog_array << self 
+
   end 
 end
